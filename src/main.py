212454321699a829ce08b516a7c132af7cffb14c
martonbeno <martonbeno459@gmail.com>
2021-02-22 16:33:37 +0100

    init

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import sys
+import re
+import traceback
+
+from helper import *
+
+def normalize(s):
+	ret = s
+	ret = re.sub(r'\\r', '', ret)
+	ret = re.sub(r'\\\\n', '\n', ret)
+	# ret = re.sub(r'\\t', ' ', ret)
+	return ret
+
+try:
+	code = normalize(sys.argv[1])
+	
+	code = code.replace(r'\n', '\n')
+	print("/%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+	print(code)
+	tree = code2tree(code)
+	treant = to_treant(tree)
+	analysis = analyze(tree)
+	ret = {
+		'analysis': analysis,
+		'treant': treant
+	}
+	print(json.dumps(ret))
+	# print(json.dumps(treant))
+except:
+	traceback.print_exc()
