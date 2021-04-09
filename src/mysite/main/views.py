2063e177d43937c4835c8f62bad8e68eece01d7d
martonbeno <martonbeno459@gmail.com>
2021-04-09 03:31:32 +0200

    táblázatban a kód és a változók

--- src/mysite/main/views.py
+++ src/mysite/main/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse, HttpRequest
 
 from .forms import CreateNewList
 from .models import Szamlalo
+from .StepPyStep import StepPyStep
+
+import json
 
 # Create your views here.
 
-SZAMLALO = Szamlalo()
+SPS_MODEL = StepPyStep()
 
 def index(response, id):
 	return render(response, "main/base.html", {'name':str(id**2)})
@@ -21,13 +24,27 @@ def create(response):
 	return render(response, "main/create.html", {'form':form})
 
 def api(request):
+	# return HttpResponse("okézsoké")
 	with open("ki.log", 'w+') as f:
+		f.write("eddigjo0s\n")
 		for k,v in request.POST.items():
-			f.write(f"{k} {v}\n")
-	try:
-		ret = int(request.POST['x']) % 2
-		SZAMLALO.inc()
-	except:
-		ret = "nincsx"
-	return HttpResponse(f"<p>{SZAMLALO.data} - {ret}<p>")
-	# return HttpResponse("<p>" + str(ret) + "<p>")
+			f.write(f"{k}: {v}\n")
+
+	
+		f.write("eddigjo111\n")
+		command = request.POST['command']
+		args = json.loads(request.POST['args'])
+		
+		
+		if command == "start":
+			source_code = args['source_code']
+			ret = SPS_MODEL.start(source_code)
+		
+		elif command == "step":
+			SPS_MODEL.request("step")
+			ret = SPS_MODEL.request("get")
+			ret = json.dumps(ret)
+			print(ret)
+	
+	
+	return HttpResponse(f"{ret}")
